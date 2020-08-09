e919103ba297a516a331d64ddd2e3c33c3a64bfd
sou412 <sou412snow@gmail.com>
2020-08-09 10:29:51 -0400

    Update documentation

--- R/attrition.R
+++ R/attrition.R
@@ -2,16 +2,31 @@
 
 #' Estimate Average Treatment Effect under Differential Attrition 
 #' 
-#' Estimate attrition scores and estimate ATE under differential attrition 
+#' Estimate attrition scores and estimate ATE under differential attrition.
 #'
 #' @export 
 #' @importFrom CBPS CBPS 
 #' @importFrom Formula as.Formula
-#' @param formula A formula of the form \code{outcome | treatmet ~  x1 + x2},
-#'                   where \code{outcoem} is the outcome of interest with missing value \code{NA}.
-#' @param data A data frame.
-#' @param estimator An estimator used for estimating ATE. Default is \code{"dr"} (double robust).
-attrition <- function(formula, data, estimator = 'dr', est_ps = FALSE) {
+#' @param formula   A formula of the form \code{outcome | treatmet ~  x1 + x2},
+#'                     where \code{outcoem} is the outcome of interest 
+#'                     with missing value \code{NA}.
+#'                     \code{treatment} is the binary treatment indicator.
+#'                     Treatment and covariates should not have missing values.
+#' @param data      A data frame. 
+#'                    It should contain all variables specified in the \code{formula}.
+#' @param estimator An estimator used for estimating ATE. 
+#'                  Default is \code{"dr"} (double robust), and the other option is 
+#'                  \code{"ipw"} (inverse probability weighting).
+#' @param est_ps    A boolean argument. If set \code{TRUE}, the propensity score is 
+#'                  estimated from the data. Otherwise, the complete randomization is  
+#'                  assumed, and the propensity score is set to the empirical propotion of 
+#'                  the treated units. 
+#'                  This option should be \code{TRUE} when the data is from
+#'                  an observational study.
+#' @param cbps      A boolean argument. If \code{TRUE}, the covariate balancing propensity
+#'                  score (CBPS) is used to estimate the attrition score (and the propensity 
+#'                  score when \code{est_ps = TRUE}). Otherwise, \code{glm} is used.
+attrition <- function(formula, data, estimator = 'dr', est_ps = FALSE, cbps = TRUE) {
   data <- as.data.frame(data)
   
   ## check if \code{outcome | treatment}
@@ -39,20 +54,11 @@ attrition <- function(formula, data, estimator = 'dr', est_ps = FALSE) {
   data$R <- R <- as.numeric(!is.na(data[,var_outcome]))
   
   ## estimate attrition score 
-  ascore <- attrition_score(update(fm_X, R ~ .), varname_treat = var_treat, data = data)
+  ascore <- attrition_score(update(fm_X, R ~ .), varname_treat = var_treat, 
+                            data = data, cbps)
   
-  ## propensity score 
-  n1 <- sum(data[,var_treat] == 1); n <- nrow(data)
-  
-  if (isTRUE(est_ps)) {
-    tmp <- capture.output({
-      fit_ps <- CBPS(fm_treat, data = data, method = 'over')  
-    })
-    ps  <- fit_ps$fitted
-  } else {
-    ps <- rep(n1 / n, n)
-  }
-
+  ## propensity score
+  ps <-  propensity_score(fm_treat, data, est_ps, cbps)
   
   ## outcome model 
   Ypred <- outcome_prediction(fm_outcome, varname_treat = var_treat, data = data)
@@ -80,27 +86,65 @@ attrition <- function(formula, data, estimator = 'dr', est_ps = FALSE) {
 #' @param formula       A formula of the form \code{response ~ x1 + x2}.
 #' @param varname_treat A character string of treatment variable name in \code{data}
 #' @param data          A data frame.
-attrition_score <- function(formula, varname_treat, data) {
+#' @param cbps          A boolean argument if CBPS is used.
+attrition_score <- function(formula, varname_treat, data, cbps) {
   
   ## compute the attrition score 
-  tmp <- capture.output(
-    fit1 <- CBPS(formula, data = filter(data, !!sym(varname_treat) == 1), method = 'over') 
-  )
-  tmp <- capture.output(
-    fit0 <- CBPS(formula, data = filter(data, !!sym(varname_treat) == 0), method = 'over')
-  )
+  if (isTRUE(cbps)) {
+    tmp <- capture.output(
+      fit1 <- CBPS(formula, data = filter(data, !!sym(varname_treat) == 1), method = 'over') 
+    )
+    tmp <- capture.output(
+      fit0 <- CBPS(formula, data = filter(data, !!sym(varname_treat) == 0), method = 'over')
+    )
+    
+    fit1_fitted <- fit1$fitted 
+    fit0_fitted <- fit0$fitted  
+  } else {
+    fit1 <- glm(formula, data = filter(data, !!sym(varname_treat) == 1), family = binomial())
+    fit0 <- glm(formula, data = filter(data, !!sym(varname_treat) == 0), family = binomial())
+    fit1_fitted <- predict(fit1, type = 'response')
+    fit0_fitted <- predict(fit0, type = 'response')
+  }
   
   ## append to the data input to ensure the ordering 
   data$attr_score <- 0 
-  data$attr_score[data[,varname_treat]==1] <- fit1$fitted
-  data$attr_score[data[,varname_treat]==0] <- fit0$fitted
+  data$attr_score[data[,varname_treat]==1] <- fit1_fitted
+  data$attr_score[data[,varname_treat]==0] <- fit0_fitted
   
   return(data$attr_score)
 }
 
-propensity_score <- function(formula, data) {
-  fit <- CBPS(formula, data, method = 'over')
-  return(fit$fit)
+
+#' Estimate propensity score 
+#' @keywords internal 
+#' @importFrom CBPS CBPS 
+#' @param formula A formula \code{treatment ~ x1 + x2}.
+#' @param data    A data frame. 
+#' @param est_ps  A boolean argument if the propensity score is estimated.
+#'                If set \code{FALSE}, empirical fraction of treated units is used.
+#' @return A vector of propensity score with length n (the number of observations).
+propensity_score <- function(formula, data, est_ps, cbps) {
+  if (isTRUE(est_ps)) {
+    if (isTRUE(cbps)) {
+      ## estimate ps with CBPS
+      tmp <- capture.output({
+        fit <- CBPS(formula, data, method = 'over')
+      })
+      ps  <- fit$fitted      
+    } else {
+      ## estimate ps with logit 
+      fit <- glm(formula, data, family = binomial())
+      ps  <- predict(fit, type = 'response')
+    }
+  } else {
+    var_treat <- all.vars(formula)[1]
+    n1 <- sum(data[,var_treat] == 1)
+    n  <- nrow(data)
+    ps <- rep(n1 / n, n)
+  }
+
+  return(ps)
 }
 
 
