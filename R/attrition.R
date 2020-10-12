4381dbddbfc696d8e64d7e6e9b8e2f81bd66d40a
sou412 <sou412snow@gmail.com>
2020-10-11 22:54:50 -0400

    Update bound function

--- R/attrition.R
+++ R/attrition.R
@@ -1,69 +1,69 @@
 
 
-#' Estimate Average Treatment Effect under Differential Attrition 
-#' 
+#' Estimate Average Treatment Effect under Differential Attrition
+#'
 #' Estimate attrition scores and estimate ATE under differential attrition.
 #'
-#' @export 
-#' @importFrom CBPS CBPS 
+#' @export
+#' @importFrom CBPS CBPS
 #' @importFrom Formula as.Formula
 #' @param formula   A formula of the form \code{outcome | treatmet ~  x1 + x2},
-#'                     where \code{outcoem} is the outcome of interest 
+#'                     where \code{outcoem} is the outcome of interest
 #'                     with missing value \code{NA}.
 #'                     \code{treatment} is the binary treatment indicator.
 #'                     Treatment and covariates should not have missing values.
-#' @param data      A data frame. 
+#' @param data      A data frame.
 #'                    It should contain all variables specified in the \code{formula}.
-#' @param estimator An estimator used for estimating ATE. 
-#'                  Default is \code{"dr"} (double robust), and the other option is 
+#' @param estimator An estimator used for estimating ATE.
+#'                  Default is \code{"dr"} (double robust), and the other option is
 #'                  \code{"ipw"} (inverse probability weighting).
-#' @param est_ps    A boolean argument. If set \code{TRUE}, the propensity score is 
-#'                  estimated from the data. Otherwise, the complete randomization is  
-#'                  assumed, and the propensity score is set to the empirical propotion of 
-#'                  the treated units. 
+#' @param est_ps    A boolean argument. If set \code{TRUE}, the propensity score is
+#'                  estimated from the data. Otherwise, the complete randomization is
+#'                  assumed, and the propensity score is set to the empirical propotion of
+#'                  the treated units.
 #'                  This option should be \code{TRUE} when the data is from
 #'                  an observational study.
 #' @param cbps      A boolean argument. If \code{TRUE}, the covariate balancing propensity
-#'                  score (CBPS) is used to estimate the attrition score (and the propensity 
+#'                  score (CBPS) is used to estimate the attrition score (and the propensity
 #'                  score when \code{est_ps = TRUE}). Otherwise, \code{glm} is used.
 attrition <- function(formula, data, estimator = 'dr', est_ps = FALSE, cbps = TRUE) {
   data <- as.data.frame(data)
-  
+
   ## check if \code{outcome | treatment}
   if (length(as.Formula(formula))[1] != 2) {
     stop('Incorrect formula specifications. See the documentation.')
   }
-  
-  ## check if all variables in the formula exists in the data 
+
+  ## check if all variables in the formula exists in the data
   vars <- all.vars(as.Formula(formula))
-  
+
   if (!all(vars %in% colnames(data))) {
     stop("Variable is missing from the data.")
   }
-  
-  ## obtain formula 
+
+  ## obtain formula
   fm_outcome <- formula(as.Formula(formula), lhs = 1, rhs = 1)
   fm_treat   <- formula(as.Formula(formula), lhs = 2, rhs = 1)
   fm_X       <- formula(as.Formula(formula), lhs = 0, rhs = 1)
-  
-  ## create missing outcome 
+
+  ## create missing outcome
   var_outcome <- vars[1]
   var_treat   <- vars[2]
-  
-  ## create attrition indicator (R = 1 if observed) 
+
+  ## create attrition indicator (R = 1 if observed)
   data$R <- R <- as.numeric(!is.na(data[,var_outcome]))
-  
-  ## estimate attrition score 
-  ascore <- attrition_score(update(fm_X, R ~ .), varname_treat = var_treat, 
+
+  ## estimate attrition score
+  ascore <- attrition_score(update(fm_X, R ~ .), varname_treat = var_treat,
                             data = data, cbps)
-  
+
   ## propensity score
   ps <-  propensity_score(fm_treat, data, est_ps, cbps)
-  
-  ## outcome model 
+
+  ## outcome model
   Ypred <- outcome_prediction(fm_outcome, varname_treat = var_treat, data = data)
   Yobs  <- pull(data, !!sym(var_outcome))
-  
+
   ## estimate ATE
   if (estimator == 'dr') {
     out <- attrition_dr(var_treat, R, data, Yobs, Ypred, ascore, ps)
@@ -72,100 +72,157 @@ attrition <- function(formula, data, estimator = 'dr', est_ps = FALSE, cbps = TR
   } else {
     stop("Not supported estimator")
   }
-  
+
   return(out)
-  
+
 }
 
-#' Estimate Bounds for ATE 
+#' Estimate Bounds for ATE
 #' @inheritParam attrition
 #' @export
 attrition_bound <- function(formula, data, cbps = TRUE, zeta = c(1, 1.1, 1.2)) {
   data <- as.data.frame(data)
-  
+
   ## check if \code{outcome | treatment}
   if (length(as.Formula(formula))[1] != 2) {
     stop('Incorrect formula specifications. See the documentation.')
   }
-  
-  ## check if all variables in the formula exists in the data 
+
+  ## check if all variables in the formula exists in the data
   vars <- all.vars(as.Formula(formula))
-  
+
   if (!all(vars %in% colnames(data))) {
     stop("Variable is missing from the data.")
   }
-  
-  ## obtain formula 
+
+  ## obtain formula
   fm_outcome <- formula(as.Formula(formula), lhs = 1, rhs = 1)
   fm_treat   <- formula(as.Formula(formula), lhs = 2, rhs = 1)
   fm_X       <- formula(as.Formula(formula), lhs = 0, rhs = 1)
-  
-  ## create missing outcome 
+
+  ## create missing outcome
   var_outcome <- vars[1]
   var_treat   <- vars[2]
-  
-  ## create attrition indicator (R = 1 if observed) 
+
+  ## create attrition indicator (R = 1 if observed)
   data$R <- R <- as.numeric(!is.na(data[,var_outcome]))
-  
+
   ## -------------------------------------- ##
-  ## estimate attrition score 
+  ## estimate attrition score
   ## -------------------------------------- ##
-  ascore <- attrition_score(update(fm_X, R ~ .), varname_treat = var_treat, 
+  ascore <- attrition_score(update(fm_X, R ~ .), varname_treat = var_treat,
                             data = data, cbps)
-  
-  
+
+
   ## -------------------------------------- ##
-  ## Compute bounds 
+  ## Compute bounds
   ## -------------------------------------- ##
   Yobs  <- pull(data, !!sym(var_outcome))
-  bound <- calc_psi(var_treat, R, data, Yobs, ascore, zeta)
-  
-  return(bound)
+  Dtr   <- pull(data, !!sym(var_treat))
+
+  tau   <- sum(na.omit(R[Dtr == 1] * Yobs[Dtr == 1])) / sum(Dtr == 1) -
+           sum(na.omit(R[Dtr == 0] * Yobs[Dtr == 0])) / sum(Dtr == 0)
+
+  # weights
+  w1 <- R[Dtr == 1] * (1 - ascore[Dtr == 1]) / ascore[Dtr == 1]
+  w0 <- R[Dtr == 0] * (1 - ascore[Dtr == 0]) / ascore[Dtr == 0]
+
+  # spatstat::wcdf()
+  # bound <- calc_psi(var_treat, R, data, Yobs, ascore, zeta)
+  Pw1 <- spatstat::ewcdf(Yobs[Dtr == 1], weights = w1)
+  Pw0 <- spatstat::ewcdf(Yobs[Dtr == 0], weights = w0)
+
+
+
+  ## common integral
+  # ∫[-∞, 0] P(x) dx
+  UB_1_2 <- integrate(Pw1, lower = min(Yobs[Dtr == 1], na.rm = TRUE), upper = 0, stop.on.error = FALSE)
+  UB_0_2 <- integrate(Pw0, lower = min(Yobs[Dtr == 0], na.rm = TRUE), upper = 0, stop.on.error = FALSE)
+
+
+
+  bounds <- purrr::map(zeta, function(zeta_use) {
+    ## define function
+    Pw1_ub <- function(x) { 1 - Pw1(x) / zeta_use }
+    Pw0_ub <- function(x) { 1 - Pw0(x) * zeta_use }
+    Pw1_lb <- function(x) { 1 - Pw1(x) * zeta_use }
+    Pw0_lb <- function(x) { 1 - Pw0(x) / zeta_use }
+
+    ## Upper bound ---------------------------
+    ## Pw1
+    # ∫[0, ∞] P(x) dx
+    UB_1_1 <- integrate(Pw1_ub, lower = 0, upper = max(Yobs[Dtr == 1], na.rm = TRUE), stop.on.error = FALSE)
+    UB_0_1 <- integrate(Pw0_ub, lower = 0, upper = max(Yobs[Dtr == 0], na.rm = TRUE), stop.on.error = FALSE)
+
+    ##
+    adj_UB <- (UB_1_1$value - UB_1_2$value / zeta_use) - (UB_0_1$value - UB_0_2$value * zeta_use)
+    tau_UB <- tau + adj_UB
+
+    ## Lower bound ---------------------------
+    # ∫[0, ∞] P(x) dx
+    LB_1_2 <- UB_1_2
+    LB_0_2 <- UB_0_2
+    LB_1_1 <- integrate(Pw1_lb, lower = 0, upper = max(Yobs[Dtr == 1], na.rm = TRUE), stop.on.error = FALSE)
+    LB_0_1 <- integrate(Pw0_lb, lower = 0, upper = max(Yobs[Dtr == 0], na.rm = TRUE), stop.on.error = FALSE)
+
+    ##
+    adj_LB <- (LB_1_1$value - LB_1_2$value * zeta_use) - (LB_0_1$value - LB_0_2$value / zeta_use)
+    tau_LB <- tau + adj_LB
+
+    return(c(LB = tau_LB, UB = tau_UB))
+  }) %>% do.call(rbind, .) %>% as_tibble() %>%
+  mutate(zeta = zeta)
+
+
+
+
+
+  return(bounds)
 }
 
-#' Estimate Attrition Score 
-#' @keywords internal 
+#' Estimate Attrition Score
+#' @keywords internal
 #' @importFrom CBPS CBPS
-#' @importFrom dplyr pull 
+#' @importFrom dplyr pull
 #' @importFrom rlang !! sym
 #' @param formula       A formula of the form \code{response ~ x1 + x2}.
 #' @param varname_treat A character string of treatment variable name in \code{data}
 #' @param data          A data frame.
 #' @param cbps          A boolean argument if CBPS is used.
 attrition_score <- function(formula, varname_treat, data, cbps) {
-  
-  ## compute the attrition score 
+
+  ## compute the attrition score
   if (isTRUE(cbps)) {
     tmp <- capture.output(
-      fit1 <- CBPS(formula, data = filter(data, !!sym(varname_treat) == 1), method = 'over') 
+      fit1 <- CBPS(formula, data = filter(data, !!sym(varname_treat) == 1), method = 'over')
     )
     tmp <- capture.output(
       fit0 <- CBPS(formula, data = filter(data, !!sym(varname_treat) == 0), method = 'over')
     )
-    
-    fit1_fitted <- fit1$fitted 
-    fit0_fitted <- fit0$fitted  
+
+    fit1_fitted <- fit1$fitted
+    fit0_fitted <- fit0$fitted
   } else {
     fit1 <- glm(formula, data = filter(data, !!sym(varname_treat) == 1), family = binomial())
     fit0 <- glm(formula, data = filter(data, !!sym(varname_treat) == 0), family = binomial())
     fit1_fitted <- predict(fit1, type = 'response')
     fit0_fitted <- predict(fit0, type = 'response')
   }
-  
-  ## append to the data input to ensure the ordering 
-  data$attr_score <- 0 
+
+  ## append to the data input to ensure the ordering
+  data$attr_score <- 0
   data$attr_score[data[,varname_treat]==1] <- fit1_fitted
   data$attr_score[data[,varname_treat]==0] <- fit0_fitted
-  
+
   return(data$attr_score)
 }
 
 
-#' Estimate propensity score 
-#' @keywords internal 
-#' @importFrom CBPS CBPS 
+#' Estimate propensity score
+#' @keywords internal
+#' @importFrom CBPS CBPS
 #' @param formula A formula \code{treatment ~ x1 + x2}.
-#' @param data    A data frame. 
+#' @param data    A data frame.
 #' @param est_ps  A boolean argument if the propensity score is estimated.
 #'                If set \code{FALSE}, empirical fraction of treated units is used.
 #' @return A vector of propensity score with length n (the number of observations).
@@ -176,9 +233,9 @@ propensity_score <- function(formula, data, est_ps, cbps) {
       tmp <- capture.output({
         fit <- CBPS(formula, data, method = 'over')
       })
-      ps  <- fit$fitted      
+      ps  <- fit$fitted
     } else {
-      ## estimate ps with logit 
+      ## estimate ps with logit
       fit <- glm(formula, data, family = binomial())
       ps  <- predict(fit, type = 'response')
     }
@@ -193,59 +250,59 @@ propensity_score <- function(formula, data, est_ps, cbps) {
 }
 
 
-#' Outcome prediction 
+#' Outcome prediction
 #' @keywords internal
 #' @importFrom dplyr filter
 #' @importFrom rlang !! sym
 outcome_prediction <- function(formula, varname_treat, data) {
-  fit1 <- lm(formula, data = filter(data, !!sym(varname_treat) == 1)) 
+  fit1 <- lm(formula, data = filter(data, !!sym(varname_treat) == 1))
   fit0 <- lm(formula, data = filter(data, !!sym(varname_treat) == 0))
-  
+
   Y1pred <- predict(fit1, data)
   Y0pred <- predict(fit0, data)
-  
+
   return(cbind(Y1pred, Y0pred))
 }
 
 
-#' Doubly Robust Estimator 
+#' Doubly Robust Estimator
 #' @keywords internal
 attrition_dr <- function(var_treat, R, data, Yobs, Ypred, ascore, ps) {
   n <- nrow(data)
   ## estimate E[Y(1)] --------------------------------------
   D <- data[,var_treat]
   use_tr <- R == 1 & D == 1
-  # outcome regression 
+  # outcome regression
   EY1 <- Ypred[,1]
-  # augmentation 
-  EY1[use_tr] <- EY1[use_tr] + 
+  # augmentation
+  EY1[use_tr] <- EY1[use_tr] +
     (Yobs[use_tr] - Ypred[use_tr, 1]) / (ascore[use_tr] * ps[use_tr])
-  
+
   ## estimate E[Y(0)] -------------------------------------
   use_ct <- R == 1 & D == 0
-  # outcome regression 
+  # outcome regression
   EY0 <- Ypred[,2]
-  # augmentation 
-  EY0[use_ct] <- EY0[use_ct] + 
+  # augmentation
+  EY0[use_ct] <- EY0[use_ct] +
     (Yobs[use_ct] - Ypred[use_ct, 2]) / (ascore[use_ct] * (1 - ps[use_ct]))
-  
+
   ## estimate ATE -----------------------------------------
-  EY1_est <- sum(EY1) / n 
-  EY0_est <- sum(EY0) / n 
+  EY1_est <- sum(EY1) / n
+  EY0_est <- sum(EY0) / n
   ATE_est <- EY1_est - EY0_est
-  
+
   ## estimate variance ------------------------------------
-  var_est <- (sum((EY0 - EY0_est)^2) + sum((EY1 - EY1_est)^2)) / n 
+  var_est <- (sum((EY0 - EY0_est)^2) + sum((EY1 - EY1_est)^2)) / n
   std.err <- sqrt(var_est) / sqrt(n)
-  
-  ## return object 
+
+  ## return object
   out <- data.frame(
-    estimate   = ATE_est, 
-    std.error  = std.err, 
-    statistic  = ATE_est / std.err, 
+    estimate   = ATE_est,
+    std.error  = std.err,
+    statistic  = ATE_est / std.err,
     p.value    = dnorm(ATE_est / std.err)
   )
-  
+
   return(out)
 }
 
@@ -255,31 +312,31 @@ attrition_ipw <- function(var_treat, R, data, Yobs, ascore, ps) {
   ## treatment vector -------------------------------------
   D <- data[,var_treat]
   n <- nrow(data)
-  
+
   ## estimate E[Y(1)] -------------------------------------
-  use_tr <- D == 1 & R == 1 
-  EY1_est <- sum(Yobs[use_tr] / (ascore[use_tr] * ps[use_tr])) / 
+  use_tr <- D == 1 & R == 1
+  EY1_est <- sum(Yobs[use_tr] / (ascore[use_tr] * ps[use_tr])) /
                 sum(1 / (ascore[use_tr] * ps[use_tr]))
-                
+
   ## estimate E[Y(0)] -------------------------------------
   use_ct <- D == 0 & R == 1
-  EY0_est <- sum(Yobs[use_ct] / (ascore[use_ct] * (1 - ps[use_ct]))) / 
+  EY0_est <- sum(Yobs[use_ct] / (ascore[use_ct] * (1 - ps[use_ct]))) /
                 sum(1 / (ascore[use_ct] * (1 - ps[use_ct])))
 
   ## estimate ATE -----------------------------------------
   ATE_est <- EY1_est - EY0_est
-  
-  
+
+
   ## estimate variance ------------------------------------
-  var_est <- (sum(((Yobs[use_tr] - EY1_est) / (ascore[use_tr] * ps[use_tr]))^2) + 
-              sum(((Yobs[use_ct] - EY0_est) / (ascore[use_ct] * (1 - ps[use_ct])))^2)) / n 
+  var_est <- (sum(((Yobs[use_tr] - EY1_est) / (ascore[use_tr] * ps[use_tr]))^2) +
+              sum(((Yobs[use_ct] - EY0_est) / (ascore[use_ct] * (1 - ps[use_ct])))^2)) / n
   std.err <- sqrt(var_est) / sqrt(n)
-  
-  ## return object 
+
+  ## return object
   out <- data.frame(
-    estimate  = ATE_est, 
-    std.error = std.err, 
-    statistic = ATE_est / std.err, 
+    estimate  = ATE_est,
+    std.error = std.err,
+    statistic = ATE_est / std.err,
     p.value   = dnorm(ATE_est / std.err)
   )
 }
@@ -293,9 +350,9 @@ calc_psi <- function(var_treat, R, data, Yobs, ascore, zeta) {
   ## treatment vector -------------------------------------
   D <- data[,var_treat]
   n <- nrow(data)
-  
+
   ## estimate 𝞧(1,1) and 𝞧(1,0) -------------------------
-  use_tr <- D == 1 & R == 1 
+  use_tr <- D == 1 & R == 1
   n1 <- sum(D == 1)
   p11 <- sum(Yobs[use_tr] / ascore[use_tr]) / n
   p10 <- sum(Yobs[use_tr] * (1 - ascore[use_tr]) / ascore[use_tr]) / n
@@ -311,21 +368,21 @@ calc_psi <- function(var_treat, R, data, Yobs, ascore, zeta) {
   for (z in seq_along(zeta)) {
     if (zeta[z] >= 1) {
       ATE_lb[z] <- (p11 + p10 / zeta[z]) - (p01 + zeta[z] * p00)
-      ATE_ub[z] <- (p11 + zeta[z] * p10) - (p01 + p00 / zeta[z])      
+      ATE_ub[z] <- (p11 + zeta[z] * p10) - (p01 + p00 / zeta[z])
     } else {
       zeta_inv <- 1 / zeta[z]
       ATE_lb[z] <- (p11 + p10 / zeta_inv) - (p01 + zeta_inv * p00)
-      ATE_ub[z] <- (p11 + zeta_inv * p10) - (p01 + p00 / zeta_inv)            
+      ATE_ub[z] <- (p11 + zeta_inv * p10) - (p01 + p00 / zeta_inv)
     }
   }
-  
-  
+
+
   ## estimate variance ------------------------------------
-  
-  ## return object 
+
+  ## return object
   out <- list(
-    psi = c(p11, p10, p01, p00), 
-    lb  = ATE_lb, 
+    psi = c(p11, p10, p01, p00),
+    lb  = ATE_lb,
     ub  = ATE_ub
   )
   return(out)
